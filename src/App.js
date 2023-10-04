001fd0218d8ac4e7780c6788c555dd3fa2685163
Nitish <nitishtiwari9318@gmail.com>
2023-10-04 15:20:39 +0530

    food app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'   //it is important
-// import Restaurant from './component/Restaurant';
+import Restaurant from './component/Restaurant';
 ///import Todo  from './component/todoproject/todo';
 
 import { Temp } from './component/weather/temp';
@@ -7,9 +7,9 @@ import { Temp } from './component/weather/temp';
  const App = () => {        // one function and its one return type
   return (
     <>
-    <Temp/>
+    {/* <Temp/> */}
         {/* <Todo/> */}
-    {/* <Restaurant/> */}
+    <Restaurant/>
     </>
   )
 }
